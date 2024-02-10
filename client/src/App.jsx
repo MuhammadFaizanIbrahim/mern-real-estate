151f204ffcfd89efc5228127414d2d07dc5550e8
MuhammadFaizanIbrahim <mfaizanibrahim26@gmail.com>
2024-02-10 16:30:11 +0500

    Created Pages using Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { SignUp } from './Pages/SignUp';
+import { Profile } from './Pages/Profile';
+import { About } from './Pages/About';
+import  {Login } from './Pages/Login';
 
 export default function App() {
-  return <h1 className='text-red-500'>App</h1>;
+  return ( <BrowserRouter>
+<Routes>
+<Route path='/' element={<Home />}/>
+<Route path='/login' element={<Login />}/>
+<Route path='/sign-up' element={<SignUp />}/>
+<Route path='/profile' element={<Profile />}/>
+<Route path='/about' element={<About />}/>
+</Routes>
+  </BrowserRouter> )
 }
